22e535b9d53b54add10873070186409931cf060b
Michael Dietz <michael.dietz@ivoke.de>
2013-09-30 15:37:07 +0200

    fix order of deployment tasks

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -2,15 +2,15 @@
 set :user, "deploy"
 set :use_sudo, false
 set :deploy_via, :remote_cache
-set :copy_exclude, %w(.git .gitmodules .DS_Store .gitignore *.md *.sample cap composer.json composer.lock)
+set :copy_exclude, %w(.git .gitmodules .DS_Store .gitignore *.md *.sample cap composer.*)
 set :keep_releases, 5
 
 #after "deploy:setup", "deploy:setup_config"
 after "deploy:setup", "shared:make_shared_dirs"
 
 after "deploy:restart", "deploy:cleanup"
-after "deploy:update_code", "shared:make_symlinks"
-after "deploy:update_code", "db:make_config"
+after "deploy:create_symlink", "shared:make_symlinks"
+after "deploy:create_symlink", "db:make_config"
 #after "deploy", "memcached:update"
 
 # Pull in the config file
