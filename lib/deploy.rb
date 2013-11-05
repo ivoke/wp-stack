2f14933dc68da998abdcb9717a685f8c199c188a
Michael Dietz <michael.dietz@ivoke.de>
2013-11-05 14:38:59 +0100

    added development platform
    * adapted sync tasks
    * removed tabs
    * added vendor dir to copy excludes

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -2,11 +2,11 @@
 set :user, "deploy"
 set :use_sudo, false
 set :deploy_via, :remote_cache
-set :copy_exclude, %w(.git .gitmodules .DS_Store .gitignore *.md *.sample cap composer.*)
+set :copy_exclude, %w(.git .gitmodules .DS_Store .gitignore *.md *.sample vendor cap composer.*)
 set :keep_releases, 5
 
-#after "deploy:setup", "deploy:setup_config"
 after "deploy:setup", "shared:make_shared_dirs"
+after "deploy:setup", "deploy:setup_config"
 
 after "deploy:restart", "deploy:cleanup"
 after "deploy:create_symlink", "shared:make_symlinks"
--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,83 +1,99 @@
 namespace :shared do
-	task :make_shared_dirs do
-		run "if [ ! -d #{shared_path}/media ]; then mkdir #{shared_path}/media; fi"
-		run "if [ ! -d #{shared_path}/config ]; then mkdir #{shared_path}/config; fi"
-	end
-	task :make_symlinks do
-		run "if [ ! -h #{current_path}/content/media ]; then ln -s ../../../shared/media #{current_path}/content/media; fi"
-		run "if [ ! -h #{current_path}/env_#{stage}.php ]; then ln -s ../../shared/config/env_#{stage}.php #{current_path}/env_#{stage}.php; fi"
-	end
+  task :make_shared_dirs do
+    run "if [ ! -d #{shared_path}/media ]; then mkdir #{shared_path}/media; fi"
+    run "if [ ! -d #{shared_path}/config ]; then mkdir #{shared_path}/config; fi"
+  end
+  task :make_symlinks do
+    run "if [ ! -h #{current_path}/content/media ]; then ln -s ../../../shared/media #{current_path}/content/media; fi"
+    run "if [ ! -h #{current_path}/env_#{stage}.php ]; then ln -s ../../shared/config/env_#{stage}.php #{current_path}/env_#{stage}.php; fi"
+  end
 end
 
 namespace :nginx do
-	desc "Restarts nginx"
-	task :restart do
-		run "sudo /etc/init.d/nginx reload"
-	end
+  desc "Restarts nginx"
+  task :restart do
+    run "sudo /etc/init.d/nginx reload"
+  end
 end
 
 namespace :phpfpm do
-	desc" Restarts PHP-FPM"
-	task :restart do
-		run "sudo /etc/init.d/php-fpm restart"
-	end
+  desc" Restarts PHP-FPM"
+  task :restart do
+    run "sudo /etc/init.d/php-fpm restart"
+  end
 end
 
 namespace :git do
-	desc "Updates git submodule tags"
-	task :submodule_tags do
-		run "if [ -d #{shared_path}/cached-copy/ ]; then cd #{shared_path}/cached-copy/ && git submodule foreach --recursive git fetch origin --tags; fi"
-	end
+  desc "Updates git submodule tags"
+  task :submodule_tags do
+    run "if [ -d #{shared_path}/cached-copy/ ]; then cd #{shared_path}/cached-copy/ && git submodule foreach --recursive git fetch origin --tags; fi"
+  end
 end
 
 namespace :memcached do
-	desc "Restarts Memcached"
-	task :restart do
-		run "echo 'flush_all' | nc localhost 11211", :roles => [:memcached]
-	end
-	desc "Updates the pool of memcached servers"
-	task :update do
-		unless find_servers( :roles => :memcached ).empty? then
-			mc_servers = '<?php return array( "' + find_servers( :roles => :memcached ).join( ':11211", "' ) + ':11211" ); ?>'
-			run "echo '#{mc_servers}' > #{current_path}/memcached.php", :roles => :memcached
-		end
-	end
+  desc "Restarts Memcached"
+  task :restart do
+    run "echo 'flush_all' | nc localhost 11211", :roles => [:memcached]
+  end
+  desc "Updates the pool of memcached servers"
+  task :update do
+    unless find_servers( :roles => :memcached ).empty? then
+      mc_servers = '<?php return array( "' + find_servers( :roles => :memcached ).join( ':11211", "' ) + ':11211" ); ?>'
+      run "echo '#{mc_servers}' > #{current_path}/memcached.php", :roles => :memcached
+    end
+  end
 end
 
 namespace :db do
-	desc "Syncs the staging database (and uploads) from production"
-	task :sync_from_production, :roles => :web	do
-		puts "Hang on... this might take a while."
-		random = rand( 10 ** 5 ).to_s.rjust( 5, '0' )
-		p = wpdb[ :production ]
-		s = wpdb[ :staging ]
-		puts "db:sync"
-		puts stage
-		system "mysqldump -u #{p[:user]} --result-file=/tmp/wpstack-#{random}.sql -h #{p[:host]} -p#{p[:password]} #{p[:name]}"
-		puts "mysql -u #{s[:user]} -h #{s[:host]} -p#{s[:password]} #{s[:name]} < /tmp/wpstack-#{random}.sql && rm /tmp/wpstack-#{random}.sql"
-		system "mysql -u #{s[:user]} -h #{s[:host]} -p#{s[:password]} #{s[:name]} < /tmp/wpstack-#{random}.sql && rm /tmp/wpstack-#{random}.sql"
+  desc "Syncs the staging database (and uploads) from production"
 
-		puts "Database synced to staging"
+  task :sync_from_production, :roles => :web  do
+    puts "Hang on... this might take a while."
+    sync(wpdb[ :production ], wpdb[ :development ])
+    puts "Database synced to staging"
 
-		# Now to copy files
-		find_servers( :roles => :web ).each do |server|
-			system "rsync -avz --delete #{production_deploy_to}/shared/media/ #{server}:#{shared_path}/files/"
-		end
-	end
-	desc "Sets the database credentials (and other settings) in wp-config.php"
-	task :make_config do
-		set :staging_domain, '' if staging_domain.nil?
-		{:'%%WP_STAGING_DOMAIN%%' => staging_domain, :'%%WP_STAGE%%' => stage, :'%%DB_NAME%%' => wpdb[stage][:name], :'%%DB_USER%%' => wpdb[stage][:user], :'%%DB_PASSWORD%%' => wpdb[stage][:password], :'%%DB_HOST%%' => wpdb[stage][:host]}.each do |k,v|
-			run "sed -i 's/#{k}/#{v}/' #{release_path}/wp-config.php", :roles => :web
-		end
-	end
+    # Now to copy files
+    find_servers( :roles => :web ).each do |server|
+      system "scp -rp #{server}:#{production_deploy_to}/shared/media ../content/"
+    end
+  end
+
+  task :sync_from_staging, :roles => :web  do
+    puts "Hang on... this might take a while."
+    sync(wpdb[ :staging ], wpdb[ :development ])
+    puts "Database synced to staging"
+
+    # Now to copy files
+    find_servers( :roles => :web ).each do |server|
+      system "scp -rp #{server}:#{staging_deploy_to}/shared/media ../content/"
+    end
+  end
+
+  def sync(source, destination)
+    timestamp = Time.now.to_i
+    source_bk_file = "/tmp/wpstack-#{source[:host]}-#{timestamp}.sql"
+    dest_bk_file = "/tmp/wpstack-#{destination[:host]}-#{timestamp}.sql"
+
+    system "mysqldump -u #{source[:user]} --result-file=#{source_bk_file} -h #{source[:host]} -p#{source[:password]} #{source[:name]}"
+
+    system "mysqldump -u #{destination[:user]} --result-file=#{dest_bk_file} -h #{destination[:host]} -p#{destination[:password]} #{destination[:name]}"
+
+    system "mysql -u #{destination[:user]} -h #{destination[:host]} -p#{destination[:password]} #{destination[:name]} < #{source_bk_file}"
+  end
+
+
+
+  desc "Sets the database credentials (and other settings) in wp-config.php"
+  task :make_config do
+    set :staging_domain, '' if staging_domain.nil?
+    {:'%%WP_STAGING_DOMAIN%%' => staging_domain, :'%%WP_STAGE%%' => stage, :'%%DB_NAME%%' => wpdb[stage][:name], :'%%DB_USER%%' => wpdb[stage][:user], :'%%DB_PASSWORD%%' => wpdb[stage][:password], :'%%DB_HOST%%' => wpdb[stage][:host]}.each do |k,v|
+      run "sed -i 's/#{k}/#{v}/' #{release_path}/wp-config.php", :roles => :web
+    end
+  end
 end
 
 namespace :deploy do
   task :setup_config, roles: :app do
-    run "mkdir -p #{shared_path}/config"
-    run "mkdir -p #{shared_path}/media"
-
     put "#{repository_path}/env_local.php.sample", "#{shared_path}/config/env_#{stage}.php"
 
     put "RewriteEngine On\nRewriteRule (.*) current/$1", "#{deploy_to}/.htaccess"
